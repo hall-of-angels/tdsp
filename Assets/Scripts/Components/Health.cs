6181bf06da3ad77ce32dda079d3a024cc8bd699f
Hall of Angels <9732893-Hall-of-Angels@users.noreply.gitlab.com>
2022-03-23 11:19:34 +1300

    Implemented health system
    As with Mover, not much needed to be changed relative to planning for this component to be implemented. There was some trickiness with coroutine syntax, but I managed to work everything out in the end. I also added a test for the health component which simply constantly has the player take damage. This is a convenient way to test the damage function, the invincibility period, and the isDead flag all at once, so it's pretty convenient :p

---
+++ Assets/Scripts/Components/Health.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private int maxHealth = 10;
+    [SerializeField] private float invincibilityPeriod = 0.5f;
+    [SerializeField] private int currentHealth;
+    public bool isDead = false;
+    private bool currentlyInvincible = false;
+
+    void Awake()
+    {
+        currentHealth = maxHealth;
+    }
+
+    void Update () 
+    {
+        if (currentHealth <= 0) isDead = true;
+        TakeDamage(1);
+    }
+
+    private IEnumerator _TakeDamage (int _damage) 
+    {
+        if (currentlyInvincible) yield break;
+        currentHealth -= _damage;
+        currentlyInvincible = true;
+        yield return new WaitForSeconds(invincibilityPeriod);
+        currentlyInvincible = false;
+    }
+
+    public void TakeDamage (int damage) {StartCoroutine(_TakeDamage(damage));}
+}
